02602a717ad7cbcbb6a81eb2f8fe4a53508f23ac
luonishicastro <luonishcastro@gmail.com>
2023-06-09 18:25:31 -0300

    Update create_tables

--- Lista (mini)Projetos/Mercado Libre/scripts/create_tables.sql
+++ Lista (mini)Projetos/Mercado Libre/scripts/create_tables.sql
@@ -1,3 +1,38 @@
-/* Criação do Banco de Dados Utilizado */
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+-- Criação do Banco de Dados Utilizado
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
 
-CREATE DATABASE BD_MERCADOLIBRE
+DECLARE @dbname nvarchar(128)
+SET @dbname = N'BD_MERCADOLIBRE'
+
+IF EXISTS (SELECT name FROM master.sys.databases WHERE name = @dbname)
+BEGIN
+	PRINT 'db exists. Please choose another name.'
+END
+
+ELSE
+BEGIN
+	CREATE DATABASE BD_MERCADOLIBRE
+END
+
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+-- Script base para a criação de tabelas
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+/* Sempre que utilizarmos o prefixo com o nome do Banco de Dados de agora em diante, executaremos uma string SQL. Deste modo, caso seja necessário criar um Banco de Dados teste, basta somente alterar a variável @dbname */
+
+DECLARE @SQL NVARCHAR(128)
+
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+-- Criação Tabelas Fato
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+/* Tabela Customer */
+DROP TABLE IF EXISTS @dbname..CUSTOMER;
+SET 
+
+CREATE TABLE @dbname..CUSTOMER (
+	[NAME] VARCHAR(150) NOT NULL
+	, [SURNAME] VARCHAR(150) NOT NULL
+	, [SEX] CHAR NOT NULL
+	, [ADDRESS] VARCHAR(150) NOT NULL
+	, [BIRTHDAY] DATE
+);
