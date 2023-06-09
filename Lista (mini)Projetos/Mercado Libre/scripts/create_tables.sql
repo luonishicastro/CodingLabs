f35b946ad24bbaab00b39a652ef9c2433bc5a957
luonishicastro <luonishcastro@gmail.com>
2023-06-09 19:01:54 -0300

    Alterações no script de criação de tabelas no projeto do Mercado Libre (entrevista)

--- Lista (mini)Projetos/Mercado Libre/scripts/create_tables.sql
+++ Lista (mini)Projetos/Mercado Libre/scripts/create_tables.sql
@@ -1,3 +1,7 @@
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+-- SCRIPTS DDL
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+
 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
 -- Criação do Banco de Dados Utilizado
 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
@@ -16,23 +20,62 @@ BEGIN
 END
 
 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
--- Script base para a criação de tabelas
+-- Script base para utilização do Banco de Dados definido
 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
 /* Sempre que utilizarmos o prefixo com o nome do Banco de Dados de agora em diante, executaremos uma string SQL. Deste modo, caso seja necessário criar um Banco de Dados teste, basta somente alterar a variável @dbname */
 
 DECLARE @SQL NVARCHAR(128)
+SET @sql = N'USE ' + QUOTENAME(@dbname)
+
+EXEC sp_executesql @sql
+GO
 
 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
 -- Criação Tabelas Fato
 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
-/* Tabela Customer */
-DROP TABLE IF EXISTS @dbname..CUSTOMER;
-SET 
+/* Tabela Customer - entidade onde se encontram todos os usuários. */
+DROP TABLE IF EXISTS [dbo].FACT_CUSTOMER;
 
-CREATE TABLE @dbname..CUSTOMER (
-	[NAME] VARCHAR(150) NOT NULL
-	, [SURNAME] VARCHAR(150) NOT NULL
-	, [SEX] CHAR NOT NULL
-	, [ADDRESS] VARCHAR(150) NOT NULL
-	, [BIRTHDAY] DATE
+CREATE TABLE [dbo].FACT_CUSTOMER (
+	[CUSTOMER_ID] INT IDENTITY NOT NULL
+	, [CUSTOMER_NAME] VARCHAR(150) NOT NULL
+	, [CUSTOMER_SURNAME] VARCHAR(150) NOT NULL
+	, [CUSTOMER_SEX] CHAR NOT NULL
+	, [CUSTOMER_ADDRESS] VARCHAR(150) NOT NULL
+	, [CUSTOMER_BIRTHDAY] DATE NOT NULL
 );
+
+/* Tabela Item - entidade onde se encontram os produtos publicados no marketplace. */
+CREATE TABLE [dbo].FACT_ITEM (
+	[ITEM_ID] INT IDENTITY NOT NULL
+	, [PRODUCT_NAME] VARCHAR(150) NOT NULL
+	, [ITEM_STATE] AS IIF(ISNULL([END_DATE], '') = '', 'A', 'I')
+	, [END_DATE] DATETIME NULL
+	, [CATEGORY_ID] INT NOT NULL
+);
+
+/* Tabela Order - entidade que reflete as transações geradas dentro do site. */
+CREATE TABLE [dbo].[FACT_ORDER] (
+	[ORDER_ID] INT IDENTITY NOT NULL
+	, [ITEM_ID] INT NOT NULL
+	, [CUSTOMER_ID] INT NOT NULL
+);
+
+
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+-- Criação Tabelas Dimensão
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+
+-- ITEM_STATE ** Acrescimos em relação ao pedido
+-- CUSTOMER_SEX ** Acrescimos em relação ao pedido
+
+/* Tabela Category - entidade com a descrição de cada categoria. */
+CREATE TABLE [dbo].DIM_CATEGORY (
+	[CATEGORY_ID] INT IDENTITY NOT NULL
+	, [CATEGORY_DESCRIPTION] VARCHAR(150) NOT NULL
+	, [CATEGORY_PATH] VARCHAR(150) NOT NULL
+);
+
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+-- Dados Teste
+--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
