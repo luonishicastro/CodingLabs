4e28e79de1b6e71625226493753921020dc9200e
luonishicastro <luonishcastro@gmail.com>
2023-07-19 22:37:50 -0300

    remake da estrutura de arquivos das listas de python

---
+++ listas-python/basic-list-cmv/List One - Curso em Vídeo.py
@@ -0,0 +1,7 @@
+#Create a program that reads a person's name and displays a welcome message.
+
+def welcome_message():
+    name = input('What is your name?\n')
+    print('Hello {}.'.format(nome))
+    print(f'It is a pleasure to met you {name}')
+    
