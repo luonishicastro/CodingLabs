0745df378138030f40ad16528f6a9cb897682421
luonishicastro <luonishcastro@gmail.com>
2023-07-22 00:14:20 -0300

    Update List One - Curso em Vídeo.py

--- python-lists/easy-problems/List One - Curso em Vídeo.py
+++ python-lists/easy-problems/List One - Curso em Vídeo.py
@@ -1,6 +1,21 @@
-#Create a program that reads a person's name and displays a welcome message.
+exercise_num = 0
 
-def welcome_message():
-    name = input('What is your name?\n')
-    print('Hello {}.'.format(name))
-    print(f'It is a pleasure to met you {name}.')
+"""Create an algorithm using Python that reads a person's name and displays a welcome message."""
+# Function to read a person's name
+def read_name():
+    name = input('Enter you name: ')
+    return name
+
+# Function to display a welcome message
+def display_welcome_message(name):
+    print(f'Welcome, {name}, you little shit ass.')
+
+#Higher-order function
+def execute_program(read_funcion, display_function):
+    name = read_funcion()
+    display_function(name)
+
+if __name__ == '__main__' and exercise_num == 1:
+    execute_program(read_name, display_welcome_message)
+
+"""Create an algorithm using Python that reads two numbers and displays the sum between them."""
