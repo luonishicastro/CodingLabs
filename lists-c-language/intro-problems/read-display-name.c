19a158386d1820b10546d6a346690bc9da8402bd
luonishicastro <luonishcastro@gmail.com>
2023-11-27 23:47:23 -0300

    changes

--- lists-c-language/intro-problems/read-display-name.c
+++ lists-c-language/intro-problems/read-display-name.c
@@ -1,10 +1,14 @@
 // Implement a code using C language that reads a persons first and last name and displays a welcome message.
 
 #include <stdio.h>
-#include "cs50.h"
 
-int main(void)
-{
-    string answer = get_string("Whats your name? ");
-    printf("hello, %s \n", answer);
+int main(void) {
+    char name[50];
+
+    // Prompt the user for input
+    printf("Enter your name: ");
+
+    scanf("%49s", name);
+    printf("Hello, %s!\n", name);
+
 }
