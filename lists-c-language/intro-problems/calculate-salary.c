a95aa1c199104c2811cb5bc7981d8876845df220
luonishicastro <luonishcastro@gmail.com>
2023-12-11 22:39:10 -0300

    Update calculate-salary.c

--- lists-c-language/intro-problems/calculate-salary.c
+++ lists-c-language/intro-problems/calculate-salary.c
@@ -1,1 +1,15 @@
 // Implement a code using C language that reads an employee's salary and shows their new salary with a 15% increase.
+
+#include<stdio.h>
+
+int main(void){
+    float salario;
+    float salarioAjustado;
+
+    printf("Qual eh o salario do funcionaro em reais?");
+    scanf("%f", &salario);
+
+    salarioAjustado = salario * 0.15 + salario;
+
+    printf("O novo salario do funcionario sera de: %.2f", salarioAjustado);
+}
