22d9a17c34184f332103d52307fd617cedbc9426
luonishicastro <luonishcastro@gmail.com>
2025-04-15 08:52:11 -0300

    Add files via upload

--- lists-python-language/hackerhank-leetcode-problems/HackerRank - Python 1 - Hello, World!.py
+++ lists-python-language/hackerhank-leetcode-problems/HackerRank - Python 1 - Hello, World!.py
@@ -1,2 +1,11 @@
 if __name__ == '__main__':
     print("Hello, World!")
+
+'''
+Quando você roda um arquivo Python diretamente (por exemplo: python meu_script.py),
+o Python define uma variável especial chamada __name__ com o valor '__main__'.
+
+Se esse mesmo arquivo for importado como módulo dentro de outro
+script (import meu_script), o valor de __name__ será o nome do módulo ('meu_script'),
+e não '__main__'.
+'''
