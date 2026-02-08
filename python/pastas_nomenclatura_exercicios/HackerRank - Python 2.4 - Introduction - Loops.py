c69c054e9665295a64210d79f0cbd3a3d372fa20
luonishicastro <luonishcastro@gmail.com>
2026-02-08 15:59:58 -0300

    Add files via upload

---
+++ python/pastas_nomenclatura_exercicios/HackerRank - Python 2.4 - Introduction - Loops.py
@@ -0,0 +1,10 @@
+'''
+The provided code stub reads an integer, n, from STDIN. For all non-negative integers i < n, print i^2.
+
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(0, n):
+    print(i*i)
