e00a0cadb9f43d41f8673c82360d3e9c2505af5d
luonishicastro <luonishcastro@gmail.com>
2026-02-12 01:13:24 -0300

    Add files via upload

--- python/introductory_exercise_suite_01/sum_numbers.py
+++ python/introductory_exercise_suite_01/sum_numbers.py
@@ -1,7 +1,7 @@
 def main():
   num1 = int(input('first number: '))
   num2 = int(input('second number: '))
-  return f'Sum:{num1 + num2}'
+  return 'Sum:{}'.format(num1+num2)
 
 
 if __name__ == '__main__':
